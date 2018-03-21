f526828ff8f44174c089543f32da83ddf9c4d746
winstonzirjacksiii <winston.zirjacks.iii+github@gmail.com>
2018-03-20 23:09:18 -0700

    added some basic loading stuff

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import GifCard from '../components/GifCard';
 
-function GifGrid({gifs}) {
-  const gifCards = gifs.map((gifObj, index) => <GifCard key={index} gifObj={gifObj} />);
+class GifGrid extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <section>
-      <div className="m-gif-grid">
-        {gifCards}
-      </div>
-    </section>
-  );
+    this.state = {
+      loading: true,
+      loadedGifs: []
+    }
+  }
+
+  onLoad(currGif) {
+    this.setState({
+      loading: true,
+      loadedGifs: [...this.state.loadedGifs, currGif]}
+    );
+  }
+
+  componentWillUpdate() {
+    if (this.state.loadedGifs.length === this.props.gifs.length) this.setState({loading:false});
+  }
+
+  render() {
+    const gifImages = this.props.gifs.map((gifObj, index) => <img key={index} src={gifObj.src} onLoad={this.onLoad.bind(this, gifObj)} alt={`dummy loader for: ${gifObj.title}`} />),
+          gifCards = this.state.loadedGifs.map((gifObj, index) => <GifCard key={index} gifObj={gifObj} />);
+  
+    return (
+      <section>
+        <div className="m-gif-grid">
+          {gifCards}
+        </div>
+        <div className="m-hidden">
+          {gifImages}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default GifGrid;
